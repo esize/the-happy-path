8e44343939741ac4be95894ceb2a436e52d4f234
Evan Sizemore <evan@wool.homes>
2024-09-24 19:15:14 -0400

    Fixed env file location

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import createJiti from "jiti";
 import { fileURLToPath } from "node:url";
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
-jiti("./app/env");
+jiti("./src/env");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
